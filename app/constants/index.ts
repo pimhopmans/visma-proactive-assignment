33311dec0e0414f590a510f5131805e57b084912
Pim Hopmans <pim.hopmans@live.nl>
2025-02-22 17:14:03 +0100

    chore: setup project for the first time

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Front-end boilerplate'
-export const appDescription = 'Front-end boilerplate for Nuxt.js, Vite, and TypeScript'
+export const appName = 'Technical assignment'
+export const appDescription = 'Technical assignment for Visma ProActive.'
