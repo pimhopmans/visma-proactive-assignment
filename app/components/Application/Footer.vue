9e5fd57c757f3adbc810ebaa102d4458dfbd9bc2
Pim Hopmans <pim.hopmans@live.nl>
2025-02-22 17:27:22 +0100

    feat: update footer content

--- app/components/Application/Footer.vue
+++ app/components/Application/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-zinc-100 border-t border-t-zinc-200 dark:bg-zinc-700 dark:border-t-zinc-600 py-8">
-    <UContainer>
-      Footer content
+    <UContainer class="text-xs">
+      Made by Pim Hopmans 👾
     </UContainer>
   </footer>
 </template>
