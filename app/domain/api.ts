3de6370b8397f33a619b300c7d5477f613bc6535
Pim Hopmans <pim.hopmans@live.nl>
2025-03-06 17:20:46 +0100

    chore: add error handling to Pokémon api requests

---
+++ app/domain/api.ts
@@ -0,0 +1,5 @@
+export interface ApiResponse<T> {
+  data: {
+    value: T
+  }
+}
--- app/pages/pokemon/index.vue
+++ app/pages/pokemon/index.vue
@@ -1,43 +1,54 @@
 <script setup lang="ts">
+import type { ApiResponse } from '~/domain/api'
 import type { OverviewItem } from '~/domain/content'
 import type { Pokemon, PokemonDetails, PokemonResponse } from '~/domain/pokemon'
 
 // States
-const pokemon = ref<Pokemon[]>([])
-const pokemonDetails = ref<OverviewItem[]>([])
+const pokemon = ref<OverviewItem[]>([])
 const nextUrl = ref<string>('')
 const isLoading = ref<boolean>(false)
 
 // Fetch the initial list of Pokémon
 async function initialLoad() {
-  isLoading.value = true
-
-  const { data: pokemonResponse } = await usePokemonData('pokemon') as { data: { value: PokemonResponse } }
-  pokemon.value = pokemonResponse.value.results
-  nextUrl.value = pokemonResponse.value.next.replace('https://pokeapi.co/api/v2/', '')
-
-  pokemonDetails.value = await fetchPokemonDetails(pokemonResponse.value.results)
-  isLoading.value = false
+  fetchPokemon('pokemon')
 }
 
 // Load more Pokémon
-async function fetchNextPokemon() {
+async function loadMorePokemon() {
+  if (!nextUrl.value) {
+    return
+  }
+
+  fetchPokemon(nextUrl.value)
+}
+
+// Fetch Pokémon
+async function fetchPokemon(url: string) {
   isLoading.value = true
 
-  const { data: pokemonResponse } = await usePokemonData(nextUrl.value) as { data: { value: PokemonResponse } }
-  pokemon.value.push(...pokemonResponse.value.results)
-  nextUrl.value = pokemonResponse.value.next.replace('https://pokeapi.co/api/v2/', '')
+  try {
+    const { data: pokemonResponse } = await usePokemonData(url) as ApiResponse<PokemonResponse>
+    const pokemonDetails = await fetchPokemonDetails(pokemonResponse.value.results)
 
-  const newPokemonDetails = await fetchPokemonDetails(pokemonResponse.value.results)
-  pokemonDetails.value.push(...newPokemonDetails)
-
-  isLoading.value = false
+    pokemon.value.push(...pokemonDetails)
+    nextUrl.value = pokemonResponse.value.next.replace('https://pokeapi.co/api/v2/', '')
+  }
+  catch (error) {
+    // eslint-disable-next-line no-alert
+    alert('Something went wrong. Please try again later.')
+    console.error(error)
+    isLoading.value = false
+    return
+  }
+  finally {
+    isLoading.value = false
+  }
 }
 
 // Fetch Pokémon details
 async function fetchPokemonDetails(pokemon: Pokemon[]) {
   const detailsPromises = pokemon.map(async (pokemon) => {
-    const { data } = await usePokemonData(`pokemon/${pokemon.name}`) as { data: { value: PokemonDetails } }
+    const { data } = await usePokemonData(`pokemon/${pokemon.name}`) as ApiResponse<PokemonDetails>
 
     const pokemonDetails: OverviewItem = {
       id: data.value.id,
@@ -57,7 +68,7 @@ initialLoad()
 <template>
   <div class="py-8">
     <UContainer :ui="{ constrained: 'max-w-5xl' }">
-      <ContentOverview title="Pokémon" :data="pokemonDetails" :is-loading="isLoading" :fetch-next-data="fetchNextPokemon" />
+      <ContentOverview title="Pokémon" :data="pokemon" :is-loading="isLoading" :fetch-next-data="loadMorePokemon" />
     </UContainer>
   </div>
 </template>
